77f03ac7b46fe23ad00be1a56268a5fb0c85dcb4
pappumani <30828059+pappumani@users.noreply.github.com>
2017-09-01 13:35:21 +0530

    Create code1.c

---
+++ code1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the value");
+scanf("%d",&a);
+if(a<0)
+{
+printf("negatine number");
+}
+elseif(a>0)
+{
+printf("positive number");
+}
+else
+{
+printf("the value is zero");
+}
