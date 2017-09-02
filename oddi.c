4ee463d0c8bde264cfa589bb854a50512bb6e8c7
pappumani <30828059+pappumani@users.noreply.github.com>
2017-09-02 11:56:03 +0530

    Create oddi.c

---
+++ oddi.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+void main()
+{
+int num,i=1,j=10;
+ printf("Print Odd Numbers in a given range itoj:\n");
+for (num = i; num <= j; num++)
+{
+if (num % 2 == 1)
+ printf ("%d ", num);
+}
+}
