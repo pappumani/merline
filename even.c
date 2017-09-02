066d21cae6336680ff07f33360178ab498afb4fe
pappumani <30828059+pappumani@users.noreply.github.com>
2017-09-02 12:03:11 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+void main()
+{
+int num,i =10,j=20;
+printf("Print even Numbers in a given range itoj:\n");
+for (num = i; num <= j; num++)
+{
+if (num%2 == 0)
+printf ("%d ",num);
+}
+}
