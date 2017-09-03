8a969a02c8d753c34ecb61dc3582931c079fc443
pappumani <30828059+pappumani@users.noreply.github.com>
2017-09-03 12:52:43 +0530

    Create numwords.c

---
+++ numwords.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+char a[10];
+int sum =0, i;
+printf("enter the string");
+scanf("%[^\n]a",a);
+for (i = 0;a[i] != '\0';i++)
+{
+if (s[i] ==' ')
+sum++;    
+}
+printf("number of words are: %d\n", sum+1);
+}
