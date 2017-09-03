588bc8769a0845f5d48caad021ed0d1f80b7d93a
pappumani <30828059+pappumani@users.noreply.github.com>
2017-09-03 11:59:58 +0530

    Create sumodd.c

---
+++ sumodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,i=0,b;
+for(a=1;a<=15;a++)
+{
+i=i+a;
+printf("sumvalue=%d",i);
+}
+for(b=15;b<=45;b++)
+{
+while(b%2==1)
+printf("odd values="%d",b);
+}
+}
