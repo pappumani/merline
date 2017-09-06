2efb092c7327da2b8fe4ea27c258e30f5237be77
pappumani <30828059+pappumani@users.noreply.github.com>
2017-09-06 16:24:14 +0530

    Create set52.c

---
+++ set52.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() 
+{
+char a[30],b[30];
+int i=0;
+printf("\nEnter two strings :");
+gets(a);
+gets(b);
+while (a[i] ==b[i] && a[i]!= '\0')
+i++;
+if (a[i] > a[i])
+printf("a>b");
+else if (a[i] < b[i])
+printf("a < b");
+else
+printf("str1 = str2");
+}
