a5a93fe82b58ab18551895d26ad2aa852ee112a0
pappumani <30828059+pappumani@users.noreply.github.com>
2017-09-06 17:42:20 +0530

    Create set54.c

---
+++ set54.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a;
+for(a=1;a<=9;a++)
+printf("enter the value");
+sacnf("%d",&a);
+if(a>9)
+printf("you enter the number is not in range");
+else
+printf("welcome user");
+}
