916d321dd39a338e1159a9d60e8743b8a863abde
pappumani <30828059+pappumani@users.noreply.github.com>
2017-09-08 16:54:20 +0530

    Create set67.c

---
+++ set67.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+int a,squ,cub,sum;
+if(a%2==0)
+{
+squ = a*a;
+}
+else
+{
+cub = a*a*a;
+printf("square\tcube\n\n");
+printf("\t%d\t%d\n\n", a, squ, cub);
+ }
+ sum=squ+cub;
+}
