7e4a5844c87c849dec02529966d6d0889bd933d8
pappumani <30828059+pappumani@users.noreply.github.com>
2017-09-08 16:59:34 +0530

    Create set69.c

---
+++ set69.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+char t[200];
+int sum= 0, i;
+printf("enter the string:");
+scanf("%s",&t);
+for (i = 0;t[i] != '\0';i++)
+{
+if (a[i] == ' ')
+sum++;    
+}
+printf("number of words in given string are: %d\n", sum+1);
+}
